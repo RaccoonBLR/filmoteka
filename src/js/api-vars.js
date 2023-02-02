230825256055653445baed3e763e1af0cd5b05ee
Maryna-Hemb <marinagembarskaa@gmail.com>
2023-02-03 00:18:20 +0200

    feat: add structure, vars, api-vars, common styles

---
+++ src/js/api-vars.js
@@ -0,0 +1,9 @@
+const API_KEY = '992e567e43c913ea6cedbae9c8870c1e';
+
+
+const BASE_URL = 'https://api.themoviedb.org/3';
+const TREND_URL = `${BASE_URL}/trending/movie/week`;
+const SEARCH_URL = `${BASE_URL}/search/movie`;
+const MOVIE_DETAILS_URL = `${BASE_URL}/movie/`;
+
+export { API_KEY, BASE_URL, TREND_URL, SEARCH_URL, MOVIE_DETAILS_URL };
