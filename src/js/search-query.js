2c9bfcaf7e9c253aae04bd60c49ce882b8557842
Zefristype <Rojdion.a@gmail.com>
2023-02-05 14:33:05 +0100

    додавання 20-ти фільмів до локал сторедж, рендер трендових фільмів

--- src/js/search-query.js
+++ src/js/search-query.js
@@ -1,38 +1,54 @@
-import { NewTrendApi, NewSearchApi } from "./news-api"
-import filmCardMarkupCreator from "./cards-markup";
+import { NewTrendApi, NewSearchApi } from './news-api';
+import filmCardMarkupCreator from './cards-markup';
 
 const carts = document.querySelector('.container-catalog');
-const form = document.querySelector(".search");
-const searchInputEl = document.querySelector(".search__input");
+const form = document.querySelector('.search');
+const searchInputEl = document.querySelector('.search__input');
 
 const TrendApi = new NewTrendApi();
 const SearchApi = new NewSearchApi();
 
+TrendApi.fetchTrend().then(dataForCatallog => {
+  localStorage.setItem('current-movies', JSON.stringify(dataForCatallog));
+  addCards(dataForCatallog);
+});
+
 form.addEventListener('submit', onSearch);
 
 async function onSearch(e) {
-    e.preventDefault();
-    // SearchApi.resetPage();
-    SearchApi.query = e.currentTarget.elements.searchQuery.value;
-    console.log(SearchApi.query)
-
+  e.preventDefault();
+  // SearchApi.resetPage();
+  SearchApi.query = e.currentTarget.elements.searchQuery.value;
+  console.log(SearchApi.query);
+  if (!SearchApi.query) {
     try {
-        const dataForCatallog = await SearchApi.fetchSearch();
-        console.log(dataForCatallog);       
-        await SearchApi.fetchSearch().then(addCards);      
-                
+      const dataForCatallog = await TrendApi.fetchTrend();
+      console.log(dataForCatallog);
+      localStorage.setItem('current-movies', JSON.stringify(dataForCatallog));
+      await TrendApi.fetchTrend().then(addCards);
+    } catch {
+      error => console.log(error.message);
     }
-    catch {error => console.log(error.message)}
+    return;
+  }
 
+  try {
+    const dataForCatallog = await SearchApi.fetchSearch();
+    console.log(dataForCatallog);
+    localStorage.setItem('current-movies', JSON.stringify(dataForCatallog));
+    await SearchApi.fetchSearch().then(addCards);
+  } catch {
+    error => console.log(error.message);
+  }
 }
 
 function addCards(data) {
- carts.innerHTML = filmCardMarkupCreator(data)
+  carts.innerHTML = filmCardMarkupCreator(data);
 }
 
-searchInputEl.addEventListener("click", onInputClean)
+searchInputEl.addEventListener('click', onInputClean);
 
 // функція для того, щоб при повторному пошуку інпуп очищувався сам (для зручності користувача)
 function onInputClean() {
-    searchInputEl.value=""
-} 
+  searchInputEl.value = '';
+}
