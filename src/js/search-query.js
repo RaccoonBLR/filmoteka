a29d0beef92bc21139339bed223e0c4f026fd4b3
OlieHimmars <109204093+OlieHimmars@users.noreply.github.com>
2023-02-06 17:45:31 +0200

    update

--- src/js/search-query.js
+++ src/js/search-query.js
@@ -2,6 +2,7 @@ import { NewTrendApi, NewSearchApi } from './news-api';
 import filmCardMarkupCreator from './cards-markup';
 import {pagination, onResultsResetPagination} from './pagination';
 
+const hiddenWarning = document.querySelector('.search__text');
 const carts = document.querySelector('.container-catalog');
 const form = document.querySelector('.search');
 const searchInputEl = document.querySelector('.search__input');
@@ -21,8 +22,20 @@ form.addEventListener('submit', onSearch);
 async function onSearch(e) {
   e.preventDefault();
   // SearchApi.resetPage();
+
   SearchApi.query = e.currentTarget.elements.searchQuery.value;  //можливо варто додати trim()
   console.log(SearchApi.query);
+
+ // Type something
+  if (SearchApi.query === '') {
+    hiddenWarning.classList.remove('hidden');
+    hiddenWarning.textContent = 'Please type something';
+    setTimeout(function () {
+      hiddenWarning.classList.add('hidden');
+    }, 3000);
+  }
+
+
   if (!SearchApi.query) {
     try {
       const dataForCatalog = await TrendApi.fetchTrend();
@@ -40,6 +53,28 @@ async function onSearch(e) {
     console.log(dataForCatalog);
     localStorage.setItem('current-movies', JSON.stringify(dataForCatalog));
     await SearchApi.fetchSearch().then(addCards);
+  
+    //wrongSearch
+
+    if (dataForCatalog.length === 0) {
+      console.log(dataForCatalog);
+      TrendApi.fetchTrend()
+        .then(dataForCatalog => {
+          localStorage.setItem(
+            'current-movies',
+            JSON.stringify(dataForCatalog)
+          );
+          addCards(dataForCatalog);
+        })
+        .catch(console.log);
+      hiddenWarning.classList.remove('hidden');
+      hiddenWarning.textContent =
+        'Search result not successful. Enter the correct movie name and';
+      setTimeout(function () {
+        hiddenWarning.classList.add('hidden');
+      }, 3000);
+    }
+
   } catch (error) {
     console.log(error.message);
   }
