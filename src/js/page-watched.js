0860335a6e0eb0416449cb3d54de3cf90b95764e
Zefristype <Rojdion.a@gmail.com>
2023-02-07 17:46:59 +0100

    оновлення queue і watchec в library після закриття модалки

--- src/js/page-watched.js
+++ src/js/page-watched.js
@@ -5,20 +5,17 @@ const btnWatched = document.querySelector('#watched-btn');
 const btnQueue = document.querySelector('#queue-btn');
 const sectionEl = document.querySelector('.films-catalog');
 
-addWatchedMarkup();
-
 btnWatched.addEventListener('click', addWatchedMarkup);
 
-function addWatchedMarkup() {
-    const markupArr = localStorage.getItem('watched-movies');
-    btnWatched.classList.add('library_btn--active');
-    btnQueue.classList.remove('library_btn--active');
-    if(!markupArr) {
-        filmsNotFoundTwo();
-    } else {
-        const markupArrParse = JSON.parse(markupArr);
-        const strokeMarkup = cardsMarkup(markupArrParse);
-        sectionEl.innerHTML = strokeMarkup;
-    }
+export default function addWatchedMarkup() {
+  const markupArr = localStorage.getItem('watched-movies');
+  const markupArrParse = JSON.parse(markupArr);
+  btnWatched.classList.add('library_btn--active');
+  btnQueue.classList.remove('library_btn--active');
+  if (markupArrParse.length === 0) {
+    filmsNotFoundTwo();
+  } else {
+    const strokeMarkup = cardsMarkup(markupArrParse);
+    sectionEl.innerHTML = strokeMarkup;
+  }
 }
-
