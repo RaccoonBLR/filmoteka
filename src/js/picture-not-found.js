4453e8c102f9fda3dadb8a961f26ead17f4cfa0a
Maryna-Hemb <marinagembarskaa@gmail.com>
2023-02-07 13:47:07 +0200

    width for zhdun

--- src/js/picture-not-found.js
+++ src/js/picture-not-found.js
@@ -2,7 +2,7 @@ import zhdun from '../images/waiting/zdun.png';
 
 const imgNotFound = `<div class='not-found_container'>
 <h2 class="library_not-found-text">No movies have been added yet. Select the desired movies on the <a href="./index.html" class="link_to-home">home page.</a></h1>
-<img src="${zhdun}" width="400" class='img_not-found'>
+<img src="${zhdun}" width="400vw" class='img_not-found'>
     </div>`;
 
 function filmsNotFound() {
