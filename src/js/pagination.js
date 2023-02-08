67e2a29be591423c0e2fa3fb44332c04d0b52cca
OlieHimmars <109204093+OlieHimmars@users.noreply.github.com>
2023-02-08 14:42:48 +0200

    pagination styles

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,4 +1,4 @@
-import 'tui-pagination/dist/tui-pagination.css';
+//import "tui-pagination/dist/tui-pagination.css";
 import Pagination from 'tui-pagination';
 import axios from 'axios';
 import {
@@ -15,34 +15,34 @@ const loader = new Loader();
 
 const cards = document.querySelector('.container-catalog');
 const container = document.getElementById('pagination');
-const options = {
-  // default value of options
-  totalItems: 0,
-  itemsPerPage: 20,
-  visiblePages: 4,
-  page: 1,
-  centerAlign: true,
 
-  firstItemClassName: 'tui-first-child',
-  lastItemClassName: 'tui-last-child',
-  template: {
-    page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-    currentPage:
-      '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-    moveButton:
-      '<a href="#" class="tui-page-btn tui-{{type}}">' +
-      '<span class="tui-ico-{{type}}">{{type}}</span>' +
-      '</a>',
-    disabledMoveButton:
-      '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-      '<span class="tui-ico-{{type}}">{{type}}</span>' +
-      '</span>',
-    moreButton:
-      '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-      '<span class="tui-ico-ellip">...</span>' +
-      '</a>',
-  },
-  usageStatistics: false,
+const options = { // default value of options
+     totalItems: 0,
+     itemsPerPage: 20,
+     visiblePages: 5,
+     page: 1,
+    centerAlign: true,
+     
+     firstItemClassName: 'tui-first-child',
+     lastItemClassName: 'tui-last-child',
+      template: {
+            page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+                currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+                    moveButton:
+            '<a href="#" class="tui-page-btn tui-{{type}} custom-class-{{type}}">' +
+                '<span class="tui-ico-{{type}}">:::</span>' +
+                '</a>',
+                disabledMoveButton:
+            '<span class="tui-page-btn tui-is-disabled tui-{{type}} custom-class-{{type}}">' +
+                '<span class="tui-ico-{{type}}">:::</span>' +
+                '</span>',
+                moreButton:
+            '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip custom-class-{{type}}">' +
+                '<span class="tui-ico-ellip">...</span>' +
+                '</a>',
+        },
+    usageStatistics: false,
+    
 };
 
 class NewPageTrendApi {
